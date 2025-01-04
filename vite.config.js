6e955245cf2d4064d89cbf3901b766effae7edfc
MrAbhayKumawat <abhaykumawat81@gmail.com>
2025-01-04 11:42:10 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/My-PortFolio/", // YOUR REPO NAME HERE
+  plugins: [react()], 
 });
